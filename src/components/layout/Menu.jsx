81af792d8401733abb6aee00ab1d11e0ba8f1b66
Gabriel Butarelo <gabrielbutarelo16@gmail.com>
2021-12-27 17:14:57 -0300

    Initial Commit

---
+++ src/components/layout/Menu.jsx
@@ -0,0 +1,23 @@
+import './Menu.css';
+import React from 'react';
+
+const Menu = props => (
+  <aside className='Menu'>
+    <nav>
+      <ul>
+        <li>
+          <a href="#">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Sobre
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+)
+
+export default Menu;
